49fe3e5f6e29ea5267340c2102eb511882d647f4
fabbiocrux <fabbiocrux.ms@gmail.com>
2022-12-09 16:12:32 +0100

    putting some graphics

--- FTIR.R
+++ FTIR.R
@@ -21,11 +21,17 @@ files <-
 ## Creating a function to transform the data 
 data_trans <- function(path_file, material ){
   
+#  path_file = files[1]; material = c("BB")
+  
   table <- read_delim(file = path_file, skip = 2, delim = " ")
-  x <-table[1] 
+  x <-table[1]
+  names(x)  <-  c("var_x") 
   y <- names(table)[-1]
-  data <- tibble( var_x = x , var_y = y ) %>% 
-    set_names("var_x", "var_y") %>% 
+  y <- as.numeric(y)
+  
+  # Making the table
+  data <- tibble( x , var_y = y ) %>% 
+    #set_names("var_x", "var_y") %>% 
     mutate(Material = material)
   
   return(data)
@@ -38,5 +44,34 @@ PET <- data_trans(path_file = files[3], "PET" )
 
 
 ## Joining together the data------
-data <- rbind(BB, HDPE, PET) %>% set_names("Var_x", "Var_Y", "Material" )
+data <- rbind(BB, HDPE, PET) %>% set_names("Var_x", "Var_y", "Material" )
+
+
+
+
+## Making the Graphic
+data %>% 
+  ggplot(aes( x=var_y*(-1), y=var_x , color = Material)) + 
+  geom_line() +
+  facet_wrap( ~ Material , ncol=1) 
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
